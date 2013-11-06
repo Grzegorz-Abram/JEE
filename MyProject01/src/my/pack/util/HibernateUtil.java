8ffd56f33f16f65b3b11b2d57a6d7d40a1e8b583
Grzegorz Abram <grzegorz.abram.1@gmail.com>
2013-11-06 22:11:07 +0100

    ...

--- MyProject01/src/my/pack/util/HibernateUtil.java
+++ MyProject01/src/my/pack/util/HibernateUtil.java
@@ -1,23 +1,29 @@
 package my.pack.util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
 
-	static {
-		try {
-			sessionFactory = new AnnotationConfiguration().configure()
-					.buildSessionFactory();
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
+	private static SessionFactory configureSessionFactory()
+			throws HibernateException {
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.h2.cfg.xml");
+		// configuration.configure("hibernate.hsqldb.cfg.xml");
+		serviceRegistry = new ServiceRegistryBuilder().applySettings(
+				configuration.getProperties()).buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		return sessionFactory;
 	}
 
 	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
+		return configureSessionFactory();
 	}
+
 }
